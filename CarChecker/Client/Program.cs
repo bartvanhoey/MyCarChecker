db8bd5ce1089bcda1209bf138b8e7d200c7cedf3
bartvanhoey <bartvanhoey@hotmail.com>
2020-06-18 13:34:16 +0200

    MakeRegisteringWork

--- CarChecker/Client/Program.cs
+++ CarChecker/Client/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using CarChecker.Client.Data;
 
 namespace CarChecker.Client
 {
@@ -17,14 +18,19 @@ namespace CarChecker.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
 
-            builder.Services.AddHttpClient("CarChecker.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            // Configure HttpClient for use when talking to server backend
+            builder.Services.AddHttpClient("CarChecker.ServerAPI",
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
+            // Other DI services
+            // builder.Services.AddScoped<LocalVehiclesStore>();
             builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("CarChecker.ServerAPI"));
-
             builder.Services.AddApiAuthorization();
+            // builder.Services.AddScoped<AccountClaimsPrincipalFactory<RemoteUserAccount>, OfflineAccountClaimsPrincipalFactory>();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
             await builder.Build().RunAsync();
         }
